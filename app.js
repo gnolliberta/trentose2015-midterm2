d025047831f868d363ad64415b32580e8fce2cf1
Giorgio <giopbm7@hotmail.it>
2015-11-05 10:28:34 +0100

    modified app.js with load words and select answer

--- app.js
+++ app.js
@@ -9,14 +9,45 @@
  *  }
  */ 
 
+
+
 var tmpl = ' <li id="ID">' +
            '  <h3>WORD</h3>' +
            '  <h3 class="solution">SOLUTION</h3>'+
            ' </li> ';
 
+var counter=0;
+var i =0;
+
 $(document).ready(function(){
 
 
+   
+   var tmp = tmpl.replace("ID",i).replace("WORD",data [i].word_en).replace("SOLUTION",data[i].word_de);
+   
+   $(".current").html(tmp);
+   $(".solution").hide();
+
+   $(".current").click(function(){
+        $(".current").html("");
+        $(".solution").show();
+    
+    });
+    
+    $(".btn opt-correct").click(function(){
+        counter ++;
+        i++;
+    });
+    
+    $(".btn opt-incorrect").click(function(){
+        i++;
+    });
+
+
+
+    $("#tot-good").html(counter);
+    $("#tot").html(7);
+    
 });
 
 
